17c2152f3b078c8cbbb557f94d55b615643de172
Chen <Chenpeleg>
2021-05-29 20:19:22 +0300

    Graphic strings in cpp

--- CourseLesson1/Main.asm
+++ CourseLesson1/Main.asm
@@ -1,2 +1,26 @@
 .386
 .model flat
+
+.data 
+
+intarr DWORD 1000h, 2000h, 3000h, 4000h
+
+.code
+start proc
+	mov edi, OFFSET intarr
+	mov ECX, LENGTHOF intarr
+	mov EAX, 0
+
+LP:
+	add EAX, [edi]
+	add edi, TYPE intarr
+	LOOP LP
+
+	ret
+
+start endp
+end
+
+start
+
+
--- JustCppConsole/JustCppConsole.cpp
+++ JustCppConsole/JustCppConsole.cpp
@@ -9,52 +9,43 @@
 using namespace std;
 int const MAX_BUFFERSIZE = 3000;
 std::string createMainString(std::string inputString);
-char* AddOneStringToAnother(char  destenation[MAX_BUFFERSIZE], char  string2[]);
+char* catString(char  destenation[MAX_BUFFERSIZE], char  string2[]);
 int main()
 {
     std::cout << "Hello World!\n\n\n";
-    char dset [MAX_BUFFERSIZE]= "st";
-    char s2[] = "string number 1";
-    
-    
-    char* final  =  AddOneStringToAnother(dset, s2) ;
+    char mainstring [MAX_BUFFERSIZE]= "string number 1";
+    char s2[MAX_BUFFERSIZE] = "string number 1";
+     catString(mainstring, s2) ;
 
-    std::cout << "\n\n\n this Is the Result";
-    std::cout << "\n\n\n this Is the Result";
+    std::cout << "\n\n\n this Is the Result:\n" << mainstring << "\n\n\n";
+ 
     //createMainString("123");
 }
 std::string createMainString(std::string inputString) {
     char finalString[MAX_BUFFERSIZE] = "******   ";
     char stringToAdd[5] = "1234";
-    // finalString.push(inputString) = finalString+inputString + "   *******";
-    AddOneStringToAnother(finalString, stringToAdd);
+ 
+    catString(finalString, stringToAdd);
     return finalString;
 }
- char* AddOneStringToAnother (char destenation[MAX_BUFFERSIZE], char string2[]) {
-    //  std::cat  MainString
- 
-    const int finalLength = sizeof(destenation) + sizeof(string2) - 1;
-    std::cout << "\n" <<   " begin loopin through the String: \n\n\ " ;
+ char* catString (char destenation[MAX_BUFFERSIZE], char string2[]) {
+  
     int l = 0;
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 100; i++) {
         char c = destenation[i];
-        std::cout << "\n" << i << " value ";
-        std::cout << c;
+    
         if (c == '\0' || l > 0) {
             destenation[i] = string2[l];
             l++;
+            if (string2[l] == '\0' || l == sizeof(string2)){
+                break;
+            }
         }
-    }
-    std::cout << "\n" << " End of loopin through the String: \n\n\ ";
-   
-
-    //char stringPointer*= &finalString;
-    // std::strncat(MainString, stringToadd, MAX_BUFFERSIZE - strlen(stringToadd) - 1);
+       
+    } 
     return destenation;
 }
- //  if (myChar != 32){
-      //    std::cout << "\n" << i << " " + myChar;
-//       }
+ 
 
 
 
