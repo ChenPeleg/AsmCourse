c9f1064aa64945845f6bd055bab897f719564281
Chen <Chenpeleg>
2021-06-25 11:13:02 +0300

    Array sum works

--- JustCppConsole/assemblyArraySum.asm
+++ JustCppConsole/assemblyArraySum.asm
@@ -2,16 +2,19 @@
 .386
 .model flat,c
 .data
-intArray DWORD 10000h, 20000h, 30000h, 40000h
+intArray DWORD 1h, 2h, 3h, 4h
 .code
 
 arraySum proc 
-		mov edi, OFFSET intArray
-		mov ecx, LengthOF  intArray
-		mov eax, 0
-
+		mov edi, OFFSET intArray ; sets the destenation array pointer
+		mov ecx, LengthOF  intArray ;initilize the loop counter C = Counter
+		mov eax, 0 ; sets the accumeltaiver initilize for zerp
+	LP:
+		add eax, [edi]	
+		add edi, TYPE intArray
+		loop LP
 		;this is for returning to the cpp
-		mov eax,452
+		;mov eax,452
 		ret
 arraySum endp  
 end  
